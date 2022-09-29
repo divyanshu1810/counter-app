4a6639c54709a081b4bc41754d6b3a6226743f48
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-09-29 13:12:17 +0530

    :sparkles: Added Buttons
    add and sub by 100

--- src/App.js
+++ src/App.js
@@ -10,12 +10,20 @@ function App() {
   const decrement = ()=>{
     dispatch({type:'DEC'});
   }
+  const addBy = ()=>{
+    dispatch({type:'ADD',payload:100})
+  }
+  const subBy = ()=>{
+    dispatch({type:'SUB',payload:100})
+  }
   return (
     <div className="App">
       <h1>Counter App</h1>
       <h2>{counter}</h2>
+      <button onClick={subBy}>Sub by 100</button>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
+      <button onClick={addBy}>Add by 100</button>
     </div>
   );
 }
